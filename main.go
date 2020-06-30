de653f730baa59c138d24a60f38c7badf03dfd2b
Harshil Bhatia <harshilbhatia2001@gmail.com>
2020-06-30 18:25:58 +0530

    Init Block

--- main.go
+++ main.go
@@ -1,14 +1,62 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
+	"strconv"
+	"time"
 )
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+// SetHash calculates and sets block hash
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+// NewBlock creates and returns Block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+// NewTestBlock creates and returns Test Block
+func NewTestBlock() *Block {
+	return NewBlock("Test Block", []byte{})
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+// AddBlock saves provided data as a block in the blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// NewBlockchain creates a new Blockchain with Test Block
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewTestBlock()}}
+}
 
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	bc.AddBlock("Send 1 BTC to Me !!!!!!!")
+	bc.AddBlock("Send 2 more BTC to Me !!!!!!!")
 
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
